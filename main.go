b567b5a202be0534d4369b33e7108752961de9b2
Dimanchick22 <dimanchick22@gmail.com>
2024-04-29 10:29:25 +0300

    1234

--- main.go
+++ main.go
@@ -23,57 +23,57 @@ type MockServiceInfo struct {
 }
 
 func createMockServiceHandler(w http.ResponseWriter, r *http.Request) {
-	// Отправляем запрос на получение порта
-	portParamResp, err := http.Get("http://45.140.178.234:9000/port")
-	if err != nil {
-			// Обработка ошибки при выполнении запроса
-			fmt.Println("Ошибка при выполнении запроса:", err)
-			http.Error(w, "Failed to fetch port parameter", http.StatusInternalServerError)
-			return
-	}
-	defer portParamResp.Body.Close()
+    // Отправляем запрос на получение порта
+    portParamResp, err := http.Get("http://127.0.0.1:9000/port")
+    if err != nil {
+        // Обработка ошибки при выполнении запроса
+        fmt.Println("Ошибка при выполнении запроса:", err)
+        http.Error(w, "Failed to fetch port parameter", http.StatusInternalServerError)
+        return
+    }
+    defer portParamResp.Body.Close()
 
-	// Проверяем статус ответа
-	if portParamResp.StatusCode != http.StatusOK {
-			// Обработка ошибки статуса ответа
-			fmt.Println("Ошибка статуса ответа:", portParamResp.Status)
-			http.Error(w, "Failed to fetch port parameter: "+portParamResp.Status, portParamResp.StatusCode)
-			return
-	}
+    // Проверяем статус ответа
+    if portParamResp.StatusCode != http.StatusOK {
+        // Обработка ошибки статуса ответа
+        fmt.Println("Ошибка статуса ответа:", portParamResp.Status)
+        http.Error(w, "Failed to fetch port parameter: "+portParamResp.Status, portParamResp.StatusCode)
+        return
+    }
 
-	// Выводим содержимое тела ответа в консоль
-	bodyContent, err := ioutil.ReadAll(portParamResp.Body)
-	if err != nil {
-			// Обработка ошибки чтения тела ответа
-			fmt.Println("Ошибка при чтении тела ответа:", err)
-			http.Error(w, "Failed to read port parameter", http.StatusInternalServerError)
-			return
-	}
+    // Выводим содержимое тела ответа в консоль
+    bodyContent, err := ioutil.ReadAll(portParamResp.Body)
+    if err != nil {
+        // Обработка ошибки чтения тела ответа
+        fmt.Println("Ошибка при чтении тела ответа:", err)
+        http.Error(w, "Failed to read port parameter", http.StatusInternalServerError)
+        return
+    }
 
-	// Извлекаем порт из строки ответа
-	portParamStr := strings.TrimSpace(string(bodyContent))
-	parts := strings.Split(portParamStr, " ")
-	if len(parts) != 3 {
-			// Неправильный формат ответа
-			fmt.Println("Неправильный формат ответа:", portParamStr)
-			http.Error(w, "Invalid response format", http.StatusInternalServerError)
-			return
-	}
+    // Извлекаем порт из строки ответа
+    portParamStr := strings.TrimSpace(string(bodyContent))
+    parts := strings.Split(portParamStr, " ")
+    if len(parts) != 3 {
+        // Неправильный формат ответа
+        fmt.Println("Неправильный формат ответа:", portParamStr)
+        http.Error(w, "Invalid response format", http.StatusInternalServerError)
+        return
+    }
 
-	port := parts[2]
+    port := parts[2]
 
-	// Проверяем, что параметр порта не пустой
-	if port == "" {
-			http.Error(w, "Port parameter is missing", http.StatusBadRequest)
-			return
-	}
+    // Проверяем, что параметр порта не пустой
+    if port == "" {
+        http.Error(w, "Port parameter is missing", http.StatusBadRequest)
+        return
+    }
 
-	// Преобразуем строку порта в число
-	mockPort, err := strconv.Atoi(port)
-	if err != nil {
-			http.Error(w, "Invalid port parameter", http.StatusBadRequest)
-			return
-	}
+    // Преобразуем строку порта в число
+    mockPort, err := strconv.Atoi(port)
+    if err != nil {
+        http.Error(w, "Invalid port parameter", http.StatusBadRequest)
+        return
+    }
 
 	cmd := exec.Command("go", "run", "mock/mock_service.go", "--port", strconv.Itoa(mockPort))
 	if err := cmd.Start(); err != nil {
@@ -83,7 +83,7 @@ func createMockServiceHandler(w http.ResponseWriter, r *http.Request) {
 
 	mutex.Lock()
 	defer mutex.Unlock()
-	Registry[strconv.Itoa(mockPort)] = fmt.Sprintf("http://45.140.178.234:%d", mockPort)
+	Registry[strconv.Itoa(mockPort)] = fmt.Sprintf("http://localhost:%d", mockPort)
 
 	fmt.Printf("Mock service created successfully! Port: %d\n", mockPort)
 
